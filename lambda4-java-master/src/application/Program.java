b60ef24a76153552cafea68652716bba1d0c4a17
Wagner Costa <wg.o.costa@gmail.com>
2020-09-29 04:03:01 -0300

    namesInlineLambdaExpression

--- lambda4-java-master/src/application/Program.java
+++ lambda4-java-master/src/application/Program.java
@@ -2,9 +2,11 @@ package application;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import entities.Product;
+import util.UpperCaseName;
 
 public class Program {
 
@@ -17,8 +19,18 @@ public class Program {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 
-		List<String> names = list.stream().map(p -> p.getName().toUpperCase()).collect(Collectors.toList());
+
+		List<String> namesInterfaceimplementation = list.stream().map(new UpperCaseName()).collect(Collectors.toList());
 		
-		names.forEach(System.out::println);
+		Function<Product, String> func = p -> p.getName().toUpperCase();
+
+		List<String> namesDeclaredLambdaExpression = list.stream().map(func).collect(Collectors.toList());
+
+		List<String> namesMethodReferenceStatic = list.stream().map(Product::staticUpperCaseName).collect(Collectors.toList());
+		List<String> namesMethodReferenceNonStatic = list.stream().map(Product::nonStaticUpperCaseName).collect(Collectors.toList());
+
+		List<String> namesInlineLambdaExpression = list.stream().map(p -> p.getName().toUpperCase()).collect(Collectors.toList());
+		
+		namesInlineLambdaExpression.forEach(System.out::println);
 	}
 }
